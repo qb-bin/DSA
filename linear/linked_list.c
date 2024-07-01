43f869a2327ed90564f75592f1c3b7bd89ebf045
qb <83409253+qb-bin@users.noreply.github.com>
2024-07-01 23:24:00 +0530

    free_list() added

--- linear/linked_list.c
+++ linear/linked_list.c
@@ -10,6 +10,20 @@ node* new_list() {
     return (node *)malloc(sizeof(node));
 }
 
+int free_list(node *head) {
+    node *temp_ptr;
+    int tracker = 0;
+    
+    while(head) {
+        temp_ptr = head->next;
+        free(head);
+        head = temp_ptr;
+        tracker++;
+    }
+    
+    return tracker;
+}
+
 int main() {
     return 0;
 }
